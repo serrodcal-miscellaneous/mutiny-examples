8da0767d8b122821c90b92cd2fe6e91a5899ffd2
SERGIO RODRIGUEZ CALVO <SERGIO.RODRIGUEZ.CALVO@ing.com>
2021-07-25 16:13:57 +0200

    exercise 1

--- src/main/java/com/serrodcal/Main.java
+++ src/main/java/com/serrodcal/Main.java
@@ -1,8 +1,22 @@
 package com.serrodcal;
 
+import io.smallrye.mutiny.Uni;
+
 public class Main {
 
-    public static void main(String... args) {
-        System.out.println("hello");
+    public static void main(String[] args) {
+
+        //Exercise 1: hello Mutiny!
+        helloMutiny();
     }
+
+    private static void helloMutiny() {
+        Uni.createFrom().item("hello")
+            .onItem().transform(item -> item + " mutiny")
+            .onItem().transform(String::toUpperCase)
+            .subscribe().with(
+                item -> System.out.println(">> " + item)
+            );
+    }
+
 }
