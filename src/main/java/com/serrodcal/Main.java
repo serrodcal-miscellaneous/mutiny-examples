5128ce7d7fea73e2796e968d9fc3739a5a48d694
SERGIO RODRIGUEZ CALVO <SERGIO.RODRIGUEZ.CALVO@ing.com>
2021-07-25 16:22:17 +0200

    exercise 2

--- src/main/java/com/serrodcal/Main.java
+++ src/main/java/com/serrodcal/Main.java
@@ -8,6 +8,10 @@ public class Main {
 
         //Exercise 1: hello Mutiny!
         helloMutiny();
+
+        //Exercise 2: Mutiny uses a builder API
+        builderAPI();
+
     }
 
     private static void helloMutiny() {
@@ -17,6 +21,22 @@ public class Main {
             .subscribe().with(
                 item -> System.out.println(">> " + item)
             );
+    }
+
+    private static void builderAPI() {
+        Uni<String> uni1 = Uni.createFrom().item("hello");
+        Uni<String> uni2 = uni1.onItem().transform(item -> item + " mutiny");
+        Uni<String> uni3 = uni2.onItem().transform(String::toUpperCase);
+
+        uni3.subscribe().with(item -> System.out.println(">> " + item));
+
+        //Note that the previous program is not equivalent to the code below
+        Uni<String> uni = Uni.createFrom().item("hello");
+
+        uni.onItem().transform(item -> item + " mutiny");
+        uni.onItem().transform(String::toUpperCase);
+
+        uni.subscribe().with(item -> System.out.println(">> " + item));
     }
 
 }
